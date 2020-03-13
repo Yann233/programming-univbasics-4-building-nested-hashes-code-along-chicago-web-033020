15cf180fd8a18bf5f88906060de600945347780a
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-13 01:14:33 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,15 +15,15 @@ def third_challenge
         :status=>"alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         :name=>"Lord Capulet",
-        :age=>"50"       
+        :age=>"50"
         },
         :matriarch => {
         :name=>"Lady Capulet",
-        :age=>"51"         
+        :age=>"51"
         },
       :heroine => {
         :name=>"Juliet",

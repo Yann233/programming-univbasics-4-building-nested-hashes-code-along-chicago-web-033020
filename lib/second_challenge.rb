285b44b6d25193c500cc6c60d7666f93e091ffc0
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-13 01:09:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -16,3 +16,20 @@ def second_challenge
   }
 end
 =end
+
+def second_challenge
+  epic_tragedy = {
+    montague: {
+      patriarch: {},
+      matriarch: {},
+      hero: {},
+      hero_friends: []
+    },
+    capulet: {
+      patriarch: {},
+      matriarch: {},
+      heroine: {},
+      heroine_friends: []
+    }
+  }
+end
